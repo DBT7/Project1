bf4475097565157be7279d559694903d587085b9
Jacob <christensen.r.jacob@gmail.com>
2015-09-26 13:22:49 -0500

    Adding Simple HTML

--- DBTeam7/cr_resource/views.py
+++ DBTeam7/cr_resource/views.py
@@ -1,13 +1,13 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
 from .models import ConferenceRoom
+
 
 def index(request):
     latest_room_list = ConferenceRoom.objects.order_by('-conference_room_seating')[:5]
-    output = ', '.join([p.conference_room_name for p in latest_room_list])
-    return HttpResponse(output)
+    context = {'latest_room_list': latest_room_list,}
+    return render(request, 'cr_resource/index.html', context)
+
 
 def detail(request, conference_room_name):
-    return HttpResponse("Your looking at conference room : {}".format(conference_room_name))
-
-
+    conference_room = get_object_or_404(ConferenceRoom ,pk=conference_room_name)
+    return render(request, 'cr_resource/detail.html', {'conference_room':conference_room})

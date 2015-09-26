5b78659854094c34d9d202a9a7c1b939f3d7c41a
Jacob <christensen.r.jacob@gmail.com>
2015-09-26 09:51:04 -0500

    Continue the tutorial

--- DBTeam7/cr_resource/views.py
+++ DBTeam7/cr_resource/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import ConferenceRoom
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the cr_resource index.")
+    latest_room_list = ConferenceRoom.objects.order_by('-conference_room_seating')[:5]
+    output = ', '.join([p.conference_room_name for p in latest_room_list])
+    return HttpResponse(output)
 
 def detail(request, conference_room_name):
-    return HttpResponse(conference_room_name)
+    return HttpResponse("Your looking at conference room : {}".format(conference_room_name))
+
 

19ac0b45e71c7adb68b9f474ac62d1bd478cc562
Jacob <christensen.r.jacob@gmail.com>
2015-11-16 22:21:27 -0600

    Remove discussed unneeded models

--- testsite/roomSchedule/models.py
+++ testsite/roomSchedule/models.py
@@ -22,16 +22,6 @@ class Address(models.Model):
         
         db_table = 'address'
 
-
-class Admin(models.Model):
-    admin_id = models.IntegerField(db_column='Admin_id', primary_key=True)  # Field name made lowercase.
-    person_person = models.ForeignKey('Person', db_column='Person_Person_id')  # Field name made lowercase.
-
-    class Meta:
-      
-        db_table = 'Admin'
-
-
 class Building(models.Model):
     building_id = models.AutoField(db_column='Building_id', primary_key=True)  # Field name made lowercase.
     name = models.CharField(db_column='Name', max_length=45, blank=True, null=True)  # Field name made lowercase.
@@ -41,69 +31,17 @@ class Building(models.Model):
         db_table = 'building'
 
 
-class Manager(models.Model):
-    manager_id = models.IntegerField(db_column='Manager_id', primary_key=True)  # Field name made lowercase.
-    admin_admin = models.ForeignKey(Admin, db_column='Admin_Admin_id')  # Field name made lowercase.
-    person_person = models.ForeignKey('Person', db_column='Person_Person_id')  # Field name made lowercase.
-
-    class Meta:
-        
-        db_table = 'Manager'
-
-
-
-class Duration(models.Model):
-    duration_id = models.AutoField(db_column='Duration_id', primary_key=True)  # Field name made lowercase.
-    duration = models.IntegerField(db_column='Duration', blank=True, null=True)  # Field name made lowercase.
-
-    class Meta:
-        db_table = 'duration'
-
-
-class Organization(models.Model):
-    organization_id = models.AutoField(db_column='Organization_id', primary_key=True)  # Field name made lowercase.
-    orgname = models.CharField(db_column='OrgName', max_length=45, blank=True, null=True)  # Field name made lowercase.
-
-    class Meta:
-        
-        db_table = 'organization'
-
-
-class Person(models.Model):
-    person_id = models.IntegerField(db_column='Person_id', primary_key=True)  # Field name made lowercase.
-    firstname = models.CharField(db_column='FirstName', max_length=45, blank=True, null=True)  # Field name made lowercase.
-    lastname = models.CharField(db_column='LastName', max_length=45, blank=True, null=True)  # Field name made lowercase.
-    email = models.CharField(db_column='Email', max_length=45, blank=True, null=True)  # Field name made lowercase.
-
-    class Meta:
-        
-        db_table = 'Person'
-
-
-class Recurrence(models.Model):
-    recurrence_id = models.AutoField(db_column='Recurrence_id', primary_key=True)  # Field name made lowercase.
-    description = models.CharField(db_column='Description', max_length=45, blank=True, null=True)  # Field name made lowercase.
-    reservation_reservation = models.ForeignKey('Reservation', db_column='Reservation_Reservation_id', blank=True, null= True)  # Field name made lowercase.
-
-    class Meta:
-        
-        db_table = 'recurrence'
-
-
-
 class Reservation(models.Model):
     reservation_id = models.AutoField(db_column='Reservation_id', primary_key=True)  # Field name made lowercase.
     reservation_dt = models.DateTimeField(db_column='Reservation_dt')  # Field name made lowercase.
-
-         
     duration = models.IntegerField(db_column='Duration', blank=True, null=True)  # Field name made lowercase.
     user_user = models.ForeignKey(auth.models.User)  # Field name made lowercase.
     room_room = models.ForeignKey('Room', db_column='Room_Room_id')  # Field name made lowercase.
     room_building_building = models.ForeignKey('Room', db_column='Room_Building_Building_id',
          related_name='reservation_room', blank=True, null=True)  # Field name made lowercase
+    resource_id = models.ForeignKey('Resource', db_column='resource_id', blank=True, null=True)
+    reservation_comment_id = models.ForeignKey('Comment', db_column='reservation_comment_id', blank=True, null=True)
 
-
-    
 
     class Meta:
         
@@ -111,29 +49,9 @@ class Reservation(models.Model):
 
 
 class Resource(models.Model):
-    room_room = models.ForeignKey('Room', db_column='Room_Room_id',related_name="res_room")  # Field name made lowercase.
-    room_building_building = models.ForeignKey('Room', db_column='Room_Building_Building_id',related_name='res_build')  # Field name made lowercase.
-    projector = models.IntegerField(db_column='Projector', blank=True, null=True)  # Field name made lowercase.
-    internet = models.IntegerField(db_column='Internet', blank=True, null=True)  # Field name made lowercase.
-    handicap_access = models.IntegerField(db_column='Handicap Access', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters.
-    chalkboard = models.IntegerField(db_column='Chalkboard', blank=True, null=True)  # Field name made lowercase.
-
-    class Meta:
-        
-        db_table = 'Resource'
-        unique_together = ['room_room', 'room_building_building']
-
-   
-
-    class Meta:
-        db_table = 'reservation'
-
-
-class Resource(models.Model):
-    resouce_id = models.AutoField(db_column='Resouce_id', primary_key=True)  # Field name made lowercase.
+    resource_id = models.AutoField(db_column='Resource_id', primary_key=True)  # Field name made lowercase.
+    title = models.CharField(db_column='Title', max_length=200, blank=True, null=True)  # Field name made lowercase.
     description = models.CharField(db_column='Description', max_length=45, blank=True, null=True)  # Field name made lowercase.
-    room_room = models.ForeignKey('Room', db_column='Room_Room_id', related_name='resource_room')  # Field name made lowercase.
-    room_building_building = models.ForeignKey('Room', db_column='Room_Building_Building_id')  # Field name made lowercase.
 
     class Meta:
         db_table = 'resource'
@@ -151,26 +69,10 @@ class Room(models.Model):
         db_table = 'room'
         unique_together = (('room_id', 'building_building'),)
 
-
-
-class User(models.Model):
-    user_id = models.IntegerField(db_column='User_id', primary_key=True)  # Field name made lowercase.
-    activereservations = models.IntegerField(db_column='ActiveReservations', blank=True, null=True)  # Field name made lowercase.
-    manager_manager = models.ForeignKey(Manager, db_column='Manager_Manager_id')  # Field name made lowercase.
-    person_person = models.ForeignKey(Person, db_column='Person_Person_id')  # Field name made lowercase.
-    organization_organization = models.ForeignKey(Organization, db_column='Organization_Organization_id')  # Field name made lowercase.
+class Comment(models.Model):
+    comment_id = models.AutoField(primary_key=True)
+    text = models.TextField(max_length=1024)
 
     class Meta:
-        
-        db_table = 'User'
 
-
-class DjangoMigrations(models.Model):
-    app = models.CharField(max_length=255)
-    name = models.CharField(max_length=255)
-    applied = models.DateTimeField()
-
-    class Meta:
-        
-        db_table = 'django_migrations'
-
+        db_table = 'comment'
